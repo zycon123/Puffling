e2db0eb93deae4052e8428b6f8cf1855f2e8fe6d
zycon123 <stian-t-95@hotmail.com>
2026-09-08 11:38:32 +0200

    Load split leaderboard UI and game state modules

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 (function(){
-const parts=['js/dom_refs.js','js/part2.js','js/boss_multiplayer.js','js/music_bridge.js','js/leaderboard_submit.js','js/part5_1.js','js/part5_helpers.js','js/part5_2.js','js/part5_3.js','js/part5_4.js'];
+const parts=['js/dom_refs.js','js/part2.js','js/boss_multiplayer.js','js/music_bridge.js','js/leaderboard_submit.js','js/leaderboard_language_ui.js','js/state_content.js','js/part5_helpers.js','js/part5_2.js','js/part5_3.js','js/part5_4.js'];
 let i=0;
 function next(){
  if(i>=parts.length)return;
