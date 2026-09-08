36c3e0c0b6c2e6cc5b980c45d468b506a88003e3
zycon123 <stian-t-95@hotmail.com>
2026-09-09 01:07:17 +0200

    Hide mission UI

---
+++ js/mission_ui_remove.js
@@ -0,0 +1,6 @@
+(function(){
+ const mission=document.getElementById('mission');
+ const complete=document.getElementById('missionComplete');
+ if(mission)mission.style.display='none';
+ if(complete)complete.style.display='none';
+})();
