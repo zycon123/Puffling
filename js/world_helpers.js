a7069e8f716433c337d493c9fee69ac6019b883b
zycon123 <stian-t-95@hotmail.com>
2026-09-08 13:26:47 +0200

    Spawn rare advanced powerups

--- js/world_helpers.js
+++ js/world_helpers.js
@@ -5,11 +5,7 @@ function addPlatform(y){
  platforms.push({x,y,w,h:16,phase:Math.random()*6.28,move:Math.random()<.24,breakable:Math.random()<.11,used:false});
  if(Math.random()<.6)coinItems.push({x:x+w/2,y:y-30,r:10,taken:false,spin:Math.random()*6});
  if(Math.random()<.12)powerups.push({x:x+w/2,y:y-43,type:['shield','magnet','mega'][Math.floor(Math.random()*3)],taken:false});
+ if(Math.random()<.035){const rare=['coinRush','superShield','overcharge'][Math.floor(Math.random()*3)];powerups.push({x:x+w/2,y:y-58,type:rare,taken:false,rare:true});}
  if(Math.random()<.11)enemies.push({x:Math.random()*(W-60)+30,y:y-90,vx:(Math.random()<.5?-1:1)*(1+Math.random()*1.1),phase:Math.random()*6.28,r:18});
 }
-function setMission(){
- missionIndex=Math.floor(Math.random()*missions.length);
- const m=missions[missionIndex];
- missionEl.textContent=tr('mission')+': '+missionText(m);
- missionDone=false;
-}
+function setMission(){missionIndex=Math.floor(Math.random()*missions.length);const m=missions[missionIndex];missionEl.textContent=tr('mission')+': '+missionText(m);missionDone=false;}
