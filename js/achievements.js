584620ebadc1a354410bea70e3fb4afd2c383403
zycon123 <stian-t-95@hotmail.com>
2026-09-08 13:43:30 +0200

    Add endless achievements and permanent rewards

---
+++ js/achievements.js
@@ -0,0 +1,24 @@
+(function(){
+ const unlockedThisRun=new Set();
+ function unlock(key,title,reward){
+  if(save[key])return false;
+  save[key]=true;save.bank+=reward;persist();refreshMenu();
+  showToast(`Achievement: ${title}! +${reward} 🪙 🏅`);
+  return true;
+ }
+ function checkHeight(){
+  if(score>=10000&&!save.achSkyLegend&&!unlockedThisRun.has('skyLegend')){
+   unlockedThisRun.add('skyLegend');unlock('achSkyLegend','SKY LEGEND',1000);
+  }
+ }
+ function eventCleared(){
+  save.eventsCleared=(save.eventsCleared||0)+1;persist();
+  if(save.eventsCleared>=5)unlock('achEventMaster','EVENT MASTER',750);
+ }
+ function bossDefeated(tier){
+  if((tier||1)>=3)unlock('achBossHunter','BOSS HUNTER',1250);
+ }
+ const previousUpdate=update;
+ update=function(dt){previousUpdate(dt);if(running)checkHeight();};
+ window.skyPuffAchievements={eventCleared,bossDefeated,checkHeight};
+})();
