18b8b8945a9845230c62a9eeac7fe8531bb25bbc
zycon123 <stian-t-95@hotmail.com>
2026-09-08 14:07:56 +0200

    Add Zycon Studios beta support contact

--- js/beta_config.js
+++ js/beta_config.js
@@ -1,4 +1,5 @@
-var SKY_PUFF_VERSION='5.26-beta.1';
+var SKY_PUFF_VERSION='5.26-beta.2';
 var SKY_PUFF_BETA=true;
+var SKY_PUFF_SUPPORT_EMAIL='zyconstudios@protonmail.com';
 var API_BASE=(localStorage.skyPuffApiBase||'').trim().replace(/\/$/,'');
-window.skyPuffConfig={version:SKY_PUFF_VERSION,beta:SKY_PUFF_BETA,get apiBase(){return API_BASE;},setApiBase(url){API_BASE=String(url||'').trim().replace(/\/$/,'');localStorage.skyPuffApiBase=API_BASE;return API_BASE;}};
+window.skyPuffConfig={version:SKY_PUFF_VERSION,beta:SKY_PUFF_BETA,supportEmail:SKY_PUFF_SUPPORT_EMAIL,get apiBase(){return API_BASE;},setApiBase(url){API_BASE=String(url||'').trim().replace(/\/$/,'');localStorage.skyPuffApiBase=API_BASE;return API_BASE;}};
