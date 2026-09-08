b3f275013a7d60bc8e51077ef43390946336d9c8
zycon123 <stian-t-95@hotmail.com>
2026-09-08 11:27:24 +0200

    Load named DOM references module

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 (function(){
-const parts=['js/part1.js','js/part2.js','js/part3.js','js/music_bridge.js','js/leaderboard_submit.js','js/part5_1.js','js/part5_helpers.js','js/part5_2.js','js/part5_3.js','js/part5_4.js'];
+const parts=['js/dom_refs.js','js/part2.js','js/part3.js','js/music_bridge.js','js/leaderboard_submit.js','js/part5_1.js','js/part5_helpers.js','js/part5_2.js','js/part5_3.js','js/part5_4.js'];
 let i=0;
 function next(){
  if(i>=parts.length)return;
