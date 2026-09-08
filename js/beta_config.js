93068c4afc9ae404f40835f86077d15063110368
zycon123 <stian-t-95@hotmail.com>
2026-09-08 17:54:36 +0200

    Prepare Sky Puff 5.26 beta.4 tester release

--- js/beta_config.js
+++ js/beta_config.js
@@ -1,4 +1,4 @@
-var SKY_PUFF_VERSION='5.26-beta.3';
+var SKY_PUFF_VERSION='5.26-beta.4';
 var SKY_PUFF_BETA=true;
 var SKY_PUFF_SUPPORT_EMAIL='zyconstudios@protonmail.com';
 var API_BASE=(localStorage.skyPuffApiBase||'').trim().replace(/\/$/,'');
