a93a7f7ae5197690d8ce500566db5758f706ebc5
zycon123 <stian-t-95@hotmail.com>
2026-09-11 15:27:14 +0200

    audio: give all ten bosses active music themes

--- js/audio_core.js
+++ js/audio_core.js
@@ -6,5 +6,16 @@ function refreshAudioUI(){musicToggleEl.textContent=musicEnabled?tr('on'):tr('of
 function startMusic(){if(!musicEnabled||bossMusicId)return;bgMusicEl.volume=musicVolume;const p=bgMusicEl.play();if(p&&p.catch)p.catch(()=>{});}
 function stopMusic(){bgMusicEl.pause();}
 let bossAudioCtx=null,bossMusicTimer=null,bossMusicStep=0,bossMusicId=null,bossMusicGain=null;
-const bossMusicThemes={storm:{bpm:148,root:146.83,scale:[0,3,7,10,12,15,19,22],wave:'square'},candy:{bpm:164,root:196.00,scale:[0,4,7,11,12,16,19,23],wave:'triangle'},ice:{bpm:126,root:130.81,scale:[0,2,7,9,12,14,19,21],wave:'sine'},galaxy:{bpm:176,root:110.00,scale:[0,3,7,8,12,15,19,20,24],wave:'sawtooth'}};
+const bossMusicThemes={
+ storm:{bpm:148,root:146.83,scale:[0,3,7,10,12,15,19,22],wave:'square'},
+ candy:{bpm:164,root:196.00,scale:[0,4,7,11,12,16,19,23],wave:'triangle'},
+ ice:{bpm:126,root:130.81,scale:[0,2,7,9,12,14,19,21],wave:'sine'},
+ galaxy:{bpm:176,root:110.00,scale:[0,3,7,8,12,15,19,20,24],wave:'sawtooth'},
+ solar:{bpm:158,root:174.61,scale:[0,4,7,12,16,19,24],wave:'sawtooth'},
+ void:{bpm:118,root:98.00,scale:[0,3,6,10,12,15,18,22],wave:'sine'},
+ thunder:{bpm:172,root:123.47,scale:[0,5,7,10,12,17,19,22],wave:'square'},
+ crystal:{bpm:136,root:164.81,scale:[0,2,7,11,14,19,23],wave:'triangle'},
+ inferno:{bpm:184,root:110.00,scale:[0,3,7,10,12,15,19,22],wave:'sawtooth'},
+ cosmic:{bpm:150,root:82.41,scale:[0,3,7,11,12,15,19,23,24],wave:'sine'}
+};
 function bossFreq(root,semitone){return root*Math.pow(2,semitone/12)}
